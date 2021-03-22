f4cbcce346fd57817eb3fb61abf1b6840cc5a09d
sarvendrasingh <sarvendra.singh_cs18@gla.ac.in>
2021-03-22 17:15:05 +0530

    Login Activity complete

---
+++ app/src/main/java/com/example/food_waste_management_app/CustomerMapActivity.java
@@ -0,0 +1,14 @@
+package com.example.food_waste_management_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_map);
+    }
+}
---
+++ app/src/main/java/com/example/food_waste_management_app/NGOMapActivity.java
@@ -0,0 +1,14 @@
+package com.example.food_waste_management_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NGOMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ngo_map);
+    }
+}
