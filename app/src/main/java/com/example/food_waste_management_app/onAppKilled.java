94f37c9a70cae68306c67dade7289f27bfca5363
sarvendrasingh <sarvendra.singh_cs18@gla.ac.in>
2021-03-22 12:13:59 +0530

    on App Killed

--- app/src/main/java/com/example/food_waste_management_app/onAppKilled.java
+++ app/src/main/java/com/example/food_waste_management_app/onAppKilled.java
@@ -1,4 +1,41 @@
 package com.example.food_waste_management_app;
 
-public class onAppKilled {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import androidx.annotation.Nullable;
+
+
+public class onAppKilled extends Service {
+
+    @Nullable
+
+    @Override
+
+    public IBinder onBind(Intent intent) {
+
+        return null;
+
+    }
+
+
+
+    @Override
+
+    public void onTaskRemoved(Intent rootIntent) {
+
+        super.onTaskRemoved(rootIntent);
+
+
+
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("driversAvailable");
+
+        GeoFire geoFire = new GeoFire(ref);
+
+        geoFire.removeLocation(userId);
+
+    }
+
 }
