99856ea18e9268a8964ee011a6c43d9890417317
sarvendrasingh <sarvendra.singh_cs18@gla.ac.in>
2021-03-22 21:14:37 +0530

    Upto Map activity Completed

--- app/src/main/java/com/example/food_waste_management_app/NGOMapActivity.java
+++ app/src/main/java/com/example/food_waste_management_app/NGOMapActivity.java
@@ -2,13 +2,38 @@ package com.example.food_waste_management_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NGOMapActivity extends AppCompatActivity {
+    private GoogleMap mMap;
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
+
+    private Button mLogout, mSettings, mCollect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_map);
+
+        mLogout = (Button) findViewById(R.id.logout);
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(NGOMapActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
     }
 }
