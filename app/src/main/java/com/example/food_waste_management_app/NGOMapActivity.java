a7b8f7b0bf4d3071004b308159874a5eac1927ed
sarvendrasingh <sarvendra.singh_cs18@gla.ac.in>
2021-03-23 11:23:53 +0530

    Help in Map implementation

--- app/src/main/java/com/example/food_waste_management_app/NGOMapActivity.java
+++ app/src/main/java/com/example/food_waste_management_app/NGOMapActivity.java
@@ -12,16 +12,20 @@ import android.widget.Switch;
 
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class NGOMapActivity extends AppCompatActivity{
+public class NGOMapActivity extends AppCompatActivity implements OnMapReadyCallback{
     private GoogleMap mMap;
     Location mLastLocation;
     LocationRequest mLocationRequest;
+    private MapView mMapView;
+
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     private Switch mWorkingSwitch;
 
@@ -65,9 +69,69 @@ public class NGOMapActivity extends AppCompatActivity{
                 }
             }
         });
+
+//        Bundle mapViewBundle = null;
+//        if (savedInstanceState != null) {
+//            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+//        }
+//        mMapView = (MapView) findViewById(R.id.mapView2);
+//        mMapView.onCreate(mapViewBundle);
+//
+//        mMapView.getMapAsync(this);
     }
 
 
+//    @Override
+//    public void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//
+//        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+//        if (mapViewBundle == null) {
+//            mapViewBundle = new Bundle();
+//            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+//        }
+//
+//        mMapView.onSaveInstanceState(mapViewBundle);
+//    }
 
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        mMapView.onResume();
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        mMapView.onStart();
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        mMapView.onStop();
+//    }
+//    @Override
+//    protected void onPause() {
+//        mMapView.onPause();
+//        super.onPause();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        mMapView.onDestroy();
+//        super.onDestroy();
+//    }
+//
+//    @Override
+//    public void onLowMemory() {
+//        super.onLowMemory();
+//        mMapView.onLowMemory();
+//    }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
 }
